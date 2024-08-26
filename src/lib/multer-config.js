eea35aebe71fd442b75a443507e8de717de741cd
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-08-26 14:04:12 +0530

    Add generateToken function and multer-config file

---
+++ src/lib/multer-config.js
@@ -0,0 +1,6 @@
+// import multer from 'multer';
+
+// const storage = multer.memoryStorage(); // Or configure disk storage if needed
+// const upload = multer({ storage });
+
+// export default upload;
