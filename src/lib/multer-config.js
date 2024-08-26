ff52d9e4792c8fc02e8c5def9d2c8aa4db7e4c56
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-08-26 20:24:15 +0530

    Add next-themes package and update layout.js and globals.css

--- src/lib/multer-config.js
+++
@@ -1,6 +0,0 @@
-// import multer from 'multer';
-
-// const storage = multer.memoryStorage(); // Or configure disk storage if needed
-// const upload = multer({ storage });
-
-// export default upload;
