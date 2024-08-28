f6ef56f7e13654d63284c7773005c40af14c8514
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-08-28 13:55:59 +0530

    Create HeroSection component

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,20 @@
+import { motion } from "framer-motion";
+import React from "react";
+
+const HeroSection = () => {
+    
+  return (
+    <motion.div 
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    transition={{ duration: 1, delay: 4 }}
+    className=" h-[400px] flex flex-col pt-10 md:pt-36 text-left items-center ">
+      <div className="">
+        <h6 className="text-xl md:text-5xl">👋Hello,</h6>
+        <h3 className="text-xl md:text-5xl md:py-3 md:pl-24  capitalize">I'm Dinesh yadav,</h3>  
+      </div>
+    </motion.div>
+  );
+};
+
+export default HeroSection;
